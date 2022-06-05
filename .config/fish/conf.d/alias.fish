40407644bb0a51d9efd2459e64f8221c37c8aaff
gomasiyo <goma@goma-gz.com>
2022-06-05 17:19:01 +0900

    Add exa alias

--- .config/fish/conf.d/alias.fish
+++ .config/fish/conf.d/alias.fish
@@ -1,2 +1,15 @@
 alias pbcopy='clip.exe'
 alias vim='nvim'
+
+# ls alias
+alias ls "ls -p -G"
+alias la "ls -A"
+alias ll "ls -l"
+alias lla "ll -A"
+
+# exa alias
+if type -q exa
+  alias ll "exa -l -g --icons"
+  alias lla "ll -a"
+  alias lt 'exa -T -L 3 -a -I "node_modules|.git|.cache" --icons'
+end
